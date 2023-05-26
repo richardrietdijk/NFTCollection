d38c4d432c35028c65905e80ab6f4050c46bffb3
Richard Rietdijk <richardrietdijk@Richards-MacBook-Pro.local>
2023-05-26 10:57:31 -0500

    fix hardhat

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x7d4e96C04268DDF0232a47Cf1981A7283C516150";
 // URL to extract Metadata for a Crypto Dev NFT
-//const METADATA_URL = "https://nft-collection-gold-sigma.vercel.app/api/";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-gold-sigma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
